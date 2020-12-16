439fac26493023fd9ca345c042821b76bf7af831
VictoriaRudnik <victoria.sevryuk@gmail.com>
2020-12-16 14:45:23 +0300

    add layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <div className="figures"> </div>
+      <div className="figures_container">
+        <div className="figures_header">Figures</div>
+        <div className="figures">
+          <div className="circle"></div>
+          <div className="rectangle"></div>
+        </div>
+      </div>
+      <div className="canvas_container">
+        <div className="canvas_header">Canvas</div>
+        <div className="canvas"></div>
+      </div>
     </div>
   );
 }
