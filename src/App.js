4140d61418041654ac0ed63e28ebd35542c1ef98
VictoriaRudnik <victoria.sevryuk@gmail.com>
2020-12-18 12:31:38 +0300

    add figure selection && figure deletion

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef, useCallback } from "react";
 
 function App() {
   const figuresList = {
@@ -19,19 +19,27 @@ function App() {
     },
   };
 
+  const [figures, setFigures] = useState([]);
+  const [idFigure, setIdFigure] = useState(0);
+  const [zIndex, setZIndex] = useState(0);
+  const [selectedFigure, setSelectedFigure] = useState();
   const [canvasCoordinates, setCanvasCoordinates] = useState();
+
   const ref = useRef(null);
   useEffect(() => {
     const newCanvasCoordinates = ref.current.getBoundingClientRect();
     setCanvasCoordinates(newCanvasCoordinates);
   }, []);
 
-  const [figures, setFigures] = useState([]);
-  const [idFigure, setIdFigure] = useState(0);
-  const [zIndex, setZIndex] = useState(0);
+  useEffect(() => {
+    document.addEventListener("keydown", deleteFigure, false);
+    return () => {
+      document.removeEventListener("keydown", deleteFigure, false);
+    };
+  }, [selectedFigure, figures]);
 
   const getStyleForFigure = (dataFigure, index) => {
-    let { pageX, pageY, type, zIndex} = dataFigure;
+    let { pageX, pageY, type, zIndex } = dataFigure;
     const figure = figuresList[type];
 
     if (pageX < canvasCoordinates.x) pageX = canvasCoordinates.x;
@@ -45,12 +53,20 @@ function App() {
       pageY + figure.height >
       canvasCoordinates.y + canvasCoordinates.height
     ) {
-      pageY = canvasCoordinates.y + canvasCoordinates.height - figure.height;
+      pageY =
+        canvasCoordinates.y + canvasCoordinates.height - figure.height - 1;
+    }
+    let border = figure.border;
+    if (selectedFigure) {
+      border =
+        dataFigure.id === selectedFigure.id
+          ? "1px solid yellow"
+          : figure.border;
     }
     return {
       width: figure.width + "px",
       height: figure.height + "px",
-      border: figure.border,
+      border: border,
       backgroundColor: figure.color,
       borderRadius: figure.borderRadius,
       left: pageX,
@@ -66,7 +82,6 @@ function App() {
       e.dataTransfer.setData("id", idFigure);
       e.target.style.border = "1px solid yellow";
     }
-    console.log("id", idFigure);
   };
 
   const dragOverHandler = (e) => {
@@ -83,18 +98,15 @@ function App() {
       ? e.dataTransfer.getData("id")
       : null;
     const figure = figuresList[type];
-
-    console.log("id", id);
     let pageX = e.pageX;
     let pageY = e.pageY;
-    console.log("event", e);
 
     const dataFigure = {
       id: idFigure,
       pageX: pageX, //- (figure.width / 2)
       pageY: pageY, //  - (figure.height / 2)
       type: type,
-      zIndex: zIndex
+      zIndex: zIndex,
     };
     if (id) {
       figures.forEach((item, index) => {
@@ -106,19 +118,27 @@ function App() {
     setFigures([...figures, dataFigure]);
     setIdFigure(idFigure + 1);
     setZIndex(zIndex + 1);
-    console.log('zIndex', zIndex)
-    console.log("figures", figures);
   };
 
-  const clickHandler = (e) => {
-    //e.target.style.border = "1px solid yellow";
+  const clickHandler = (e, figure) => {
     e.target.style.zIndex = zIndex;
     setZIndex(zIndex + 1);
-  }
+    setSelectedFigure(figure);
+  };
 
-  const handleDelete = (e, idFigure) => {
-      figures.filter((figure) => figure.id !== idFigure)
-  }
+  const deleteFigure = useCallback(
+    (event) => {
+      if (selectedFigure) {
+        const idSelectedFigure = selectedFigure.id;
+        if (event.keyCode === 46) {
+          setFigures(
+            figures.filter((figure) => figure.id !== idSelectedFigure)
+          );
+        }
+      }
+    },
+    [selectedFigure, figures]
+  );
 
   return (
     <div className="App">
@@ -129,13 +149,11 @@ function App() {
             className="circle"
             onDragStart={(e) => dragStartHandler(e, "circle")}
             draggable
-            // ref={refCircle}
           ></div>
           <div
             className="rectangle"
             onDragStart={(e) => dragStartHandler(e, "rectangle")}
             draggable
-            //ref={refRectangle}
           ></div>
         </div>
       </div>
@@ -152,19 +170,16 @@ function App() {
       {figures.map((figure, index) => {
         return (
           <div
-            className="canvas"
+            className="figure"
             onDragOver={(e) => dragOverHandler(e)}
             onDrop={(e) => dragDropHandler(e)}
             key={index}
             style={getStyleForFigure(figure, index)}
             onDragStart={(e) => dragStartHandler(e, figure.type, figure.id)}
             onDragEnd={(e) => dragEndHandler(e)}
-            onClick={(e) => clickHandler(e)}
-            onDelete={(e) => handleDelete(e, figure.id)}
+            onClick={(e) => clickHandler(e, figure)}
             draggable
-          >
-            
-          </div>
+          ></div>
         );
       })}
     </div>
